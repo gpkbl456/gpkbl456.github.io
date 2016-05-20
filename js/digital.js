09699cd9cac50814248646581976a6f010431630
gpkbl456 <tv3977@gmail.com>
2016-05-20 14:41:32 +0800

    e
    e

--- js/digital.js
+++ js/digital.js
@@ -5,9 +5,9 @@ function num_btn(num) {
    count++;
 };
 function cut() {
-  var div = document.getElementById("box" + (count-1));
+  count - 1;
+  var div = document.getElementById("box" + count);
    div.textContent = "";
-   count - 2;
 };
 function ok() {
   count = 1;
