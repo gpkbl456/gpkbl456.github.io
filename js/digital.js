74f6b8a972ff08b751796ac746b4bddf1a5dea25
gpkbl456 <tv3977@gmail.com>
2016-05-20 14:50:42 +0800

    e
    e

--- js/digital.js
+++ js/digital.js
@@ -5,9 +5,22 @@ function num_btn(num) {
    count++;
 };
 function cut() {
-  var div = document.getElementById("box" + (count - 1));
-  div.textContent = "";
-  count - 1;
+  if (count == 4) {
+    var div = document.getElementById("box" + count);
+     div.textContent = "";
+     count -1 ;
+  }else if (count == 3) {
+    var div = document.getElementById("box" + count);
+     div.textContent = "";
+     count -1 ;
+  }else if (count == 2) {
+    var div = document.getElementById("box" + count);
+     div.textContent = "";
+     count -1 ;
+  }else if (count == 1) {
+    var div = document.getElementById("box" + count);
+     div.textContent = "";
+  }
 };
 function ok() {
   count = 1;
