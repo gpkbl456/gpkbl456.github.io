a3f3498830de8d9fe977f3d4ebed529c6019af7d
gpkbl456 <tv3977@gmail.com>
2016-05-20 14:32:26 +0800

    e
    e

--- js/digital.js
+++ js/digital.js
@@ -1,4 +1,6 @@
+var count = 1;
 function num_btn(num) {
-  var div = document.getElementById("box1");
+  var div = document.getElementById("box" + count);
    div.textContent = num;
+   count++;
 };
